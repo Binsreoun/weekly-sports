10d2d46f77f4ddcf19c26c7f3ee0c09d93e6e30f
emost22 <emoney96@naver.com>
2023-11-22 09:46:47 +0900

    Feat: Add custom response

---
+++ src/main/java/com/weekly/sports/model/dto/response/SampleRes.java
@@ -0,0 +1,16 @@
+package com.weekly.sports.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SampleRes {
+
+    private String name;
+    private String text;
+}
