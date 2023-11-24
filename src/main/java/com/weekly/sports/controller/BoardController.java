9b5bffb801ada53a388cf3784d289b581257fda9
jeendale <jeekoo7203@gmail.com>
2023-11-24 20:14:54 +0900

    fix: Boardservice클래스 공통된 부분 메서드로 따로생성(getBoardEntity)아이디 없으면 에러 반환 feat: 게시글 삭제

--- src/main/java/com/weekly/sports/controller/BoardController.java
+++ src/main/java/com/weekly/sports/controller/BoardController.java
@@ -6,12 +6,15 @@ import com.weekly.sports.model.dto.response.BoardResponseDto;
 import com.weekly.sports.service.BoardService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,5 +48,12 @@ public class BoardController {
   public BoardResponseDto updateBoard(@PathVariable Long boardId,
       @RequestBody BoardUpdateRequestDto requestDto) {
     return boardService.updateBoard(boardId, requestDto);
+  }
+
+  //게시글 삭제
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @DeleteMapping("/{boardId}")
+  public void deleteBoard(@PathVariable Long boardId) {
+    boardService.deleteBoard(boardId);
   }
 }
--- src/main/java/com/weekly/sports/service/BoardService.java
+++ src/main/java/com/weekly/sports/service/BoardService.java
@@ -18,20 +18,21 @@ public class BoardService {
   private final BoardRepository boardRepository;
 
 
+  //게시글 작성service
   public BoardResponseDto addBoard(BoardAddRequestDto requestDto) {
     BoardEntity boardEntity = new BoardEntity(requestDto);
     BoardEntity saveBoard = boardRepository.save(boardEntity);
     return new BoardResponseDto(saveBoard);
   }
 
+  //게시글 단일 조회 service
   public BoardResponseDto getBoard(Long boardId) {
     //Id없는 경우
-    BoardEntity boardEntity = boardRepository.findById(boardId)
-        .orElseThrow(() -> new NullPointerException("해당 게시글을 찾을 수 업습니다."));
-
+    BoardEntity boardEntity = getBoardEntity(boardId);
     return new BoardResponseDto(boardEntity);
   }
 
+  //게시글 전체 조회 service
   public List<BoardResponseDto> getBoards() {
     List<BoardResponseDto> responseDtoList = new ArrayList<>();
 
@@ -44,14 +45,28 @@ public class BoardService {
     return responseDtoList;
   }
 
+  //게시글 수정 service
+  //인증 과정 빠진 상태입니다.
   @Transactional //데이터 변경 감지시 자동으로 변경
   public BoardResponseDto updateBoard(Long boardId, BoardUpdateRequestDto requestDto) {
-    BoardEntity boardEntity = boardRepository.findById(boardId)
-        .orElseThrow(() -> new NullPointerException("해당 게시글을 찾을 수 업습니다."));
+    BoardEntity boardEntity = getBoardEntity(boardId);
 
-    //인증 없이 바로 된다.
     boardEntity.update(requestDto);
 
     return new BoardResponseDto(boardEntity);
+  }
+
+  //게시글 삭제 service
+  //인증 과정 빠진 상태입니다.
+  public void deleteBoard(Long boardId) {
+    BoardEntity boardEntity = getBoardEntity(boardId);
+    boardRepository.delete(boardEntity);
+  }
+
+  //공통된 부분 메서드 생성
+  //Id 확인하여 게시판 유무 확인
+  private BoardEntity getBoardEntity(Long boardId) {
+    return boardRepository.findById(boardId)
+        .orElseThrow(() -> new NullPointerException("해당 게시글을 찾을 수 업습니다."));
   }
 }
