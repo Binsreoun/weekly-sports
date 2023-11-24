e766e6eaef5ad05a032c21ed70300c81a38e9587
emost22 <emoney96@naver.com>
2023-11-24 19:04:13 +0900

    Feat: Add get user profile

--- src/main/java/com/weekly/sports/model/entity/UserEntity.java
+++ src/main/java/com/weekly/sports/model/entity/UserEntity.java
@@ -1,12 +1,15 @@
 package com.weekly.sports.model.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,4 +32,7 @@ public class UserEntity extends BaseEntity {
     private String introduction;
     @Enumerated(EnumType.STRING)
     private UserSocialEnum social;
+
+    @OneToMany(mappedBy = "followingUserId", cascade = CascadeType.ALL)
+    private List<FollowEntity> followEntities;
 }
