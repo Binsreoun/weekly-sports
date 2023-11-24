9de1241a9278ea3c2c9ae261c8dcf2382a5d5928
jeendale <jeekoo7203@gmail.com>
2023-11-24 19:21:18 +0900

    feat: 게시글 수정

--- src/main/java/com/weekly/sports/controller/BoardController.java
+++ src/main/java/com/weekly/sports/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.weekly.sports.controller;
 
 import com.weekly.sports.model.dto.request.BoardAddRequestDto;
+import com.weekly.sports.model.dto.request.BoardUpdateRequestDto;
 import com.weekly.sports.model.dto.response.BoardResponseDto;
 import com.weekly.sports.service.BoardService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,5 +38,12 @@ public class BoardController {
   @GetMapping
   public List<BoardResponseDto> getBoards() {
     return boardService.getBoards();
+  }
+
+  //게시글 수정
+  @PatchMapping("/{boardId}")
+  public BoardResponseDto updateBoard(@PathVariable Long boardId,
+      @RequestBody BoardUpdateRequestDto requestDto) {
+    return boardService.updateBoard(boardId, requestDto);
   }
 }
--- src/main/java/com/weekly/sports/service/BoardService.java
+++ src/main/java/com/weekly/sports/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.weekly.sports.service;
 
 import com.weekly.sports.model.dto.request.BoardAddRequestDto;
+import com.weekly.sports.model.dto.request.BoardUpdateRequestDto;
 import com.weekly.sports.model.dto.response.BoardResponseDto;
 import com.weekly.sports.model.entity.BoardEntity;
 import com.weekly.sports.repository.BoardRepository;
@@ -8,12 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
   private final BoardRepository boardRepository;
+
 
   public BoardResponseDto addBoard(BoardAddRequestDto requestDto) {
     BoardEntity boardEntity = new BoardEntity(requestDto);
@@ -39,5 +42,16 @@ public class BoardService {
     }
 
     return responseDtoList;
+  }
+
+  @Transactional //데이터 변경 감지시 자동으로 변경
+  public BoardResponseDto updateBoard(Long boardId, BoardUpdateRequestDto requestDto) {
+    BoardEntity boardEntity = boardRepository.findById(boardId)
+        .orElseThrow(() -> new NullPointerException("해당 게시글을 찾을 수 업습니다."));
+
+    //인증 없이 바로 된다.
+    boardEntity.update(requestDto);
+
+    return new BoardResponseDto(boardEntity);
   }
 }
