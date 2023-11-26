d45d0ae5d9fbf24240249004da7e41dc8e9906ab
RyanDJang <jangd6995@gmail.com>
2023-11-26 10:02:21 +0900

    comment 초안

---
+++ src/main/java/com/weekly/sports/ryanJang/user/UserDTO.java
@@ -0,0 +1,13 @@
+package com.weekly.sports.ryanJang.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String username;
+    public UserDTO(User user){
+        this.username = user.getUsername();
+    }
+}
